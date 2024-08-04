f62571220bdc6e89265918a24158879929cce2bd
Obarief <oktavariba@gmail.com>
2024-08-04 20:48:26 +0700

    menambahkan materi tentang class dan percobaan menggunakan node untuk export require

--- Nodejs/coba.js
+++ Nodejs/coba.js
@@ -1,5 +1,43 @@
+// function
 function cetakNama(n){
     return `halo nama saya ${n}`
 }
 
-module.exports = cetakNama;
+// variabel
+const PI = 3.14;
+
+// object
+const mahasiswa = {
+    nama: 'okta',
+    umur: 20,
+    cetakMhs(){
+        return `nama saya ${this.nama} dan umur saya ${this.umur} tahun.`
+    }
+}
+
+// class
+class Orang { // aturan dalam pembuatan kelas itu huruf besar
+    constructor(){ // constructor adalah ketika class orang diinstansiasi maka jalankan ......
+        console.log('objek orang telah dibuat')
+    }
+}
+
+
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI
+// module.exports.mahasiswa = mahasiswa
+// module.exports.Orang = Orang
+
+// .PI dan .cetakNama bisa bebas namanya namun untuk agar tidak membingungkan maka samakan saja
+
+// kita bisa mengeksport tidak dengan berulang kali seperti diatas dengan cara menjdikan object
+// module.exports = {
+//     cetakNama: cetakNama,
+//     PI: PI,
+//     mahasiswa: mahasiswa,
+//     Orang: Orang
+// };
+
+// untuk lebih simple lagi jika nama key dan valuesnya sama maka tulis sekali saja (ES6)
+module.exports = {cetakNama, PI, mahasiswa, Orang};
--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,3 +1,10 @@
-const cetakNama =  require('./coba')
+// const fs = require('fs') // untuk core modules
+// const cetakNama =  require('./coba') // local modules import (karena ada ./...)
+// const nodemon = require('nodemon') // NPM
 
-console.log(cetakNama('okta'))
+
+const coba = require('./coba')
+// file coba menjadi sebuah object sehingga pemanggilan valuesnya menggunakan coba.(nama keynya)
+
+console.log(coba.cetakNama('okta'), coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang)
+// dapat memanggil function, variabel, object, class
