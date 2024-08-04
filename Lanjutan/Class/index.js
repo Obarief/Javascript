36f8a65c2152a7cef58f0716e61638ef1d554762
Obarief <oktavariba@gmail.com>
2024-08-04 21:05:07 +0700

    menambah materi this/constructor dan new

---
+++ Lanjutan/Class/index.js
@@ -0,0 +1,7 @@
+class Orang { // aturan dalam pembuatan kelas itu huruf besar
+    constructor(){ // constructor adalah ketika class orang diinstansiasi maka jalankan jadikan object
+        console.log('objek orang telah dibuat')
+    }
+}
+
+console.log(new Orang) // new digunakan untuk membuat baru instance(objeck) dari kelas menggunakan 
