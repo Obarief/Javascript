e89a21ae2d033c98884fded92eb0361f51d5303c
Obarief <oktavariba@gmail.com>
2024-08-06 16:08:58 +0700

    nodemon

--- Nodejs/NPM/app.js
+++ Nodejs/NPM/app.js
@@ -7,7 +7,11 @@
 // console.log(validator.isNumeric('081383966520'))
 
 
-import chalk from 'chalk';
+// import chalk from 'chalk';
+
+// const nama = 'okta';
+// const umur = 20;
+// console.log(`Ram: ${chalk.bgRed('20%')} pada komputer anda yang sedang ${chalk.bgBlack.white.strikethrough('MENYALA!')}, nama anda : ${chalk.bgRed.bold.yellow(nama)}, umur anda ${chalk.bgGreen.bold(umur)}`)
 
 
-console.log(`Ram: ${chalk.bgRed('20%')} pada komputer anda yang sedang ${chalk.bgBlack.white('MENYALA!')}`)
+// program data kontak
