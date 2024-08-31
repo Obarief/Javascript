d10484e41940b6afe0d4d5f30fc57aca2a5cfba6
Obarief <oktavariba@gmail.com>
2024-08-31 17:23:08 +0700

    +

---
+++ Lanjutan/Function/Plus.js
@@ -0,0 +1,24 @@
+// function declaration
+function halo() {
+    console.log('halo dunia')
+}
+
+// function expression
+const halo = function(){
+    console.log('halo dunia')
+} 
+
+// arrow function
+const halo = () => {
+    console.log('halo dunia')
+}
+
+// anonymous function
+() => {
+    console.log('halo dunia')
+}
+
+// IIFE (immediately invoked function expression)
+(() => {
+    console.log('halo dunia')
+})()
